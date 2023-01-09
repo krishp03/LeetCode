2f1c49100599f98a3cb25e4d074a38da6e4b2f16
Krish Patel <78389562+krishp03@users.noreply.github.com>
2023-01-09 02:48:06 -0500

    Create ValidParenthesis.java

---
+++ Grind75/Week1/ValidParenthesis.java
@@ -0,0 +1,34 @@
+class ValidParenthesis {
+    public boolean isValid(String s) {
+        Stack<Character> startBrackets = new Stack<>();
+        char[] chars = s.toCharArray();
+        for(char c : chars) {
+
+            if(isOpening(c)) {
+                startBrackets.push(c);
+            }
+
+            else {
+                if(startBrackets.isEmpty()) {
+                    return false;
+                }
+                if(!isCorresponding(startBrackets.pop(), c))
+                    return false;
+            }
+        }
+        return startBrackets.size() == 0;
+    }
+
+    private boolean isCorresponding(char open, char close) {
+        if(open == '(') {
+            return close == ')';
+        } else if(open == '{') {
+            return close == '}';
+        }
+        return close == ']';
+    }
+
+    private boolean isOpening(char c) {
+        return (c == '(' || c == '{' || c == '[');
+    }
+}
