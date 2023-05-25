de0a1ed56845928f48b700b2a3d0e4480b890469
Krish Patel <78389562+krishp03@users.noreply.github.com>
2023-05-25 01:49:16 -0400

    Create ContainsDuplicate.py

---
+++ Grind75/Week2/ContainsDuplicate.py
@@ -0,0 +1,9 @@
+class ContainsDuplicate:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        numSet = set()
+        for num in nums:
+            if num in numSet:
+                return True
+            numSet.add(num)
+        return False
+        
