a789a53fdde9c5f79229af93ca252bd119c0a0c0
Krish Patel <78389562+krishp03@users.noreply.github.com>
2023-05-25 01:01:01 -0400

    Create IsPalindrome.py

---
+++ Grind75/Week1/IsPalindrome.py
@@ -0,0 +1,17 @@
+class IsPalindrome:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+        while(True):
+            while(l < len(s)-1 and not s[l].isalnum()):
+                l+=1
+            while(r > 0 and not s[r].isalnum()):
+                r-=1
+            if(l > r):
+                return True
+            elif (s[l].upper() == s[r].upper()):
+                l+=1
+                r-=1
+            else:
+                return False
+        return True
